f57e9f719aa6eefdf10a63edb7f5c3d335872177
MGCWM <MGCWM@users.noreply.github.com>
2026-09-24 17:16:09 +0800

    chore(release): 1.13-ubuntu-r3

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.openminis.ubuntu"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1302
-        versionName = "1.13-ubuntu-r2"
+        versionCode = 1303
+        versionName = "1.13-ubuntu-r3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
